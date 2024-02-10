1052c20b224fc8a8cea19aea76fe4eb686a6f492
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2024-02-10 01:30:38 +0000

    Update uv.sw.js

--- static/uv/uv.sw.js
+++ static/uv/uv.sw.js
@@ -1,5 +1,5 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
+importScripts('UV-Static/static/uv/uv.bundle.js');
+importScripts('UV-Static/static/uv/uv.config.js');
 
 class UVServiceWorker extends EventEmitter {     
     constructor(config = __uv$config) {
