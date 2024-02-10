a36300c39d2ffab6848ac8537dfbe421cbd95458
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2024-02-10 01:39:34 +0000

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: 'UV-Static/static/tiw/',
+    prefix: 'static/tiw/',
     bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: 'UV-Static/static/uv/uv.handler.js',
-    bundle: 'UV-Static/static/uv/uv.bundle.js',
-    config: 'UV-Static/static/uv/uv.config.js',
-    sw: 'UV-Static/static/uv/uv.sw.js',
+    handler: 'static/uv/uv.handler.js',
+    bundle: 'static/uv/uv.bundle.js',
+    config: 'static/uv/uv.config.js',
+    sw: 'static/uv/uv.sw.js',
 };
